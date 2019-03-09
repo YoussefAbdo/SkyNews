eb3990b1c4f8f85f190383a94db7378a55a0aec0
YoussefAbdo <mryoussefjoe@gmail.com>
2019-03-09 18:44:25 +0200

    Initial commit

---
+++ SkyNews/SkyNews/ArticleBodyPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using SkyNews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SkyNews
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ArticleBodyPage : ContentPage
+	{
+		public ArticleBodyPage (Article article)
+		{
+			InitializeComponent ();
+            TitleLabel.Text = article.Title;
+            BodyLabel.Text = article.Body;
+
+        }
+	}
+}
--- SkyNews/SkyNews/MainPage.xaml.cs
+++ SkyNews/SkyNews/MainPage.xaml.cs
@@ -1,17 +1,32 @@
-﻿using System;
+﻿using SkyNews.Models;
+using SkyNews.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace SkyNews
 {
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
+
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : ContentPage
+	{
+        ArticlesViewModel articlesViewMode;
+
+        public MainPage ()
+		{
+			InitializeComponent ();
+            articlesViewMode = new ArticlesViewModel();
+            BindingContext = articlesViewMode;
+        }
+        private async void OnItemTapped(object sender, ItemTappedEventArgs e)
         {
-            InitializeComponent();
+            //DependencyService.Get<ArticlesViewModel>().CurrentFlag = (Flag)e.Item;
+            await Navigation.PushAsync(new ArticleBodyPage((Article)e.Item));
         }
     }
 }
